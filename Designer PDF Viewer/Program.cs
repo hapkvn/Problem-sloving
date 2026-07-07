8292b36a9cc1adb9a38d82b65d28b20a2cc80485
hapkvn <huyhoangpkvn75@gmail.com>
2026-07-07 17:08:18 +0700

    Designer PDF Viewer

---
+++ Designer PDF Viewer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr_num_word = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        
+        string word = Console.ReadLine();
+
+        int max = 0;
+
+        foreach( char c in word)
+        {
+            int temp = c - 'a';
+
+            temp = arr_num_word[temp];
+
+            max = Math.Max(max, temp);
+        }
+       
+        Console.WriteLine(max* word.Length);
+
+
+    }
+}
