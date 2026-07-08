3b356bbfe76a80805988c51ba470821929b89c8e
hapkvn <huyhoangpkvn75@gmail.com>
2026-07-08 23:10:45 +0700

    Viral Advertising

---
+++ Viral Advertising/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int n  = int.Parse(Console.ReadLine());
+        int sum= 2;
+        int temp =2;
+        for(int i=1; i< n; i++)
+        {
+            temp = temp*3/2;
+            sum +=temp;
+        }
+        Console.WriteLine(sum);
+    }
+}
