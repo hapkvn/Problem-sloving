8f8c17e3ef7c815abdc8dcbda973f97da2ce21f4
hapkvn <huyhoangpkvn75@gmail.com>
2026-07-29 23:38:25 +0700

    Service Lane

---
+++ Service Lane/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arrInput = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        int[] arrHW = Array.ConvertAll(Console.ReadLine(). Split(' '), int.Parse);
+
+        List<int[]> vehicles = new List<int[]>();
+
+        for(int i=0; i< arrInput[1];i++){
+            int[] veArr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            vehicles.Add(veArr);
+        }
+
+        foreach(int[] vehicle in vehicles)
+        {
+            int begin  = vehicle[0];
+            int end = vehicle[1];
+            int min =3;
+            for(int i=begin; i <= end; i++)
+            {
+                if (arrHW[i] < min)
+                {
+                    min = arrHW[i];
+                }
+            }
+            Console.WriteLine(min);
+            min =3;
+        }
+
+
+
+    }
+}
