2b58a22af708d4e08d438c0a7107ce74cb83247d
hapkvn <huyhoangpkvn75@gmail.com>
2026-08-03 11:42:07 +0700

    Beautiful Days at the Movies

---
+++ Beautiful Days at the Movies/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        int beautiful_day = 0;
+        int k = arr[2];
+        int begin = arr[0];
+        int end = arr[1];
+        
+        for(int i=begin; i<=end; i++)
+        {
+            int orginal = i;
+            int reversed = 0;
+
+            while (orginal > 0)
+            {
+                reversed = reversed*10 +(orginal%10);
+                orginal/=10;
+            }
+
+            if ((i-reversed) % k == 0)
+            {
+                beautiful_day++;
+            }
+        }
+        Console.WriteLine(beautiful_day);
+    }
+}
