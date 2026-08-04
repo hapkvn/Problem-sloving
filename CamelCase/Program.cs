fbc2f7c96d498c83c766c3985fdec8706aca08d4
hapkvn <huyhoangpkvn75@gmail.com>
2026-08-04 09:39:03 +0700

    CamelCase

---
+++ CamelCase/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input_s = Console.ReadLine();
+
+        char[] char_s = input_s.ToCharArray();
+        int sum =1;
+        foreach(char cs in char_s)
+        {
+            if (char.IsUpper(cs) == true)
+            {
+                sum++;
+            }
+        }
+        Console.WriteLine(sum);
+    }
+}
