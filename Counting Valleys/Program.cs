ce01aa8549247c1f9aec04cdea25ea717c23034a
hapkvn <huyhoangpkvn75@gmail.com>
2026-08-05 10:20:56 +0700

    Counting Valleys

---
+++ Counting Valleys/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int never_use = int.Parse(Console.ReadLine()); 
+        string steps = Console.ReadLine();
+
+        int altitude = 0; 
+        int valleys = 0;  
+
+        foreach (char step in steps)
+        {
+            if (step == 'U')
+            {
+                altitude++;
+                
+      
+                if (altitude == 0)
+                {
+                    valleys++;
+                }
+            }
+            else if (step == 'D')
+            {
+                altitude--;
+            }
+        }
+
+        Console.WriteLine(valleys);
+    }
+}
