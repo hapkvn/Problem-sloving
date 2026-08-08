81681323fd965b9b628d2b539ae0459f724611cb
hapkvn <huyhoangpkvn75@gmail.com>
2026-08-08 23:41:22 +0700

    Equalize the Array

---
+++ Equalize the Array/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int never_use = int.Parse(Console.ReadLine());
+        int[] main_arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        int[] temp_arr = new int[101];
+        foreach(int num in main_arr)
+        {
+            temp_arr[num]++;
+
+        }
+        int max = temp_arr.Max();
+        
+        Console.WriteLine(never_use - max);
+    }
+}
--- Subarray Division/Program.cs
+++ Subarray Division/Program.cs
@@ -4,26 +4,26 @@ class Program
 {
     static void Main()
     {
-        int n = int.Parse(Console.ReadLine());
+        int never_use = int.Parse(Console.ReadLine());
+        int[] main_array = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        int[] d_m = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 
-        int[] number = Array.ConvertAll(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries), int.Parse);
-        int[] dm = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        int d = d_m[0];
+        int m = d_m[1];
 
-        int d = dm[0];
-        int m = dm[1];
-        int kq=0;
-        for(int i=0; i<= n-m; i++)
+        int result = 0;
+        for(int i=0; i<= never_use - m ; i++)
         {
             int temp=0;
-            for(int j=0; j<m; j++)
+            for(int j=0; j< m; j++)
             {
-                    temp += number[i+j];                   
+                temp +=main_array[j+i];
             }
-            if(temp == d)
+            if( temp == d)
             {
-                kq++;
+                result++;
             }
         }
-        Console.WriteLine(kq);
+        Console.WriteLine(result);
     }
 }
