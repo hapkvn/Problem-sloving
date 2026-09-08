c31efef3424d4134574a5298c6c480e4b24163d3
hapkvn <huyhoangpkvn75@gmail.com>
2026-09-08 22:50:42 +0700

    Jumping on the Clouds Revisited

---
+++ Jumping on the Clouds Revisited/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        int nerverUse = int.Parse(Console.ReadLine());
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        int sum = 0;
+        int temp = 0;
+        for(int i = 0; i<arr.Length; i++)
+        {
+            if(arr[i] == 0)
+            {
+                sum++;
+                temp++;
+            }
+            if(arr[i] == 1 )
+            {
+                temp=0;
+
+            }
+            if (temp == 3)
+            {
+                sum--;
+                temp = 1;
+            }
+        }
+        Console.WriteLine(sum-1);
+    }
+}
