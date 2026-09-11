1e3727a4444017fd9c2973727ad63b1813732f46
hapkvn <huyhoangpkvn75@gmail.com>
2026-09-11 23:08:15 +0700

    Alternating ChaJumping on the Clouds Revisited

---
+++ Jumping on the Clouds/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    public static void Main()
+    {
+        int[] n_input =  Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        int n = n_input[0];
+        int k = n_input[1];
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        int sum = 0;
+        int ensery = 100;
+        int current = 0;
+
+        while (true)
+        {
+            current = (current + k) % n;
+
+            if(arr[current] == 1)
+            {
+                ensery-=3;
+            }
+            else
+            {
+                ensery--;
+            }
+
+            if(current == 0)
+            {
+                break;
+            }
+        }
+        
+
+        Console.WriteLine(ensery);
+    }
+}
