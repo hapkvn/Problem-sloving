59e7e3fc9519dbce4fd7d5e5108f626bd5bcdf4a
hapkvn <huyhoangpkvn75@gmail.com>
2026-09-14 02:07:10 +0700

    Cut the sticks

---
+++ Cut the sticks/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        List<int> sticks = arr.ToList();
+
+        while (sticks.Count >0)
+        {            
+            Console.WriteLine(sticks.Count);
+
+            int MinStick = sticks.Min();
+            
+                for(int i=0; i<sticks.Count; i++)
+                {
+                    sticks[i] -= MinStick;
+                }
+            sticks.RemoveAll(x => x <= 0);
+            
+        }
+        
+    }
+}
