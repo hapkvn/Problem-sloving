d25518aee27637b9ef506b080f9db422706de937
hapkvn <huyhoangpkvn75@gmail.com>
2026-09-16 14:50:34 +0700

    Sales by Match

---
+++ Sales by Match/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+        int[] temp_arr = new int[101];
+
+        foreach(int ar in arr)
+        {
+            temp_arr[ar]++;
+        }
+
+        
+        int sum =0;
+
+        for(int i=0; i<temp_arr.Length; i++)
+        {
+            temp_arr[i] = temp_arr[i]/2;
+        }
+        for(int i=0; i<temp_arr.Length; i++)
+        {
+            
+            sum += temp_arr[i];
+        }
+    
+
+        Console.WriteLine(sum);
+
+    }
+}
