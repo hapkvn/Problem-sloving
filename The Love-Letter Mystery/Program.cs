eec0bb6527dea6d8a33649931474da610950cd6b
hapkvn <huyhoangpkvn75@gmail.com>
2026-09-16 15:57:19 +0700

    The Love-Letter Mystery

---
+++ The Love-Letter Mystery/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        string[] sArr = new string[n];
+        int[] result = new int[n];
+
+        for(int i=0; i<n; i++)
+        {
+            sArr[i] = Console.ReadLine();
+        }
+        for(int i=0; i<n; i++)
+        {
+            char[] temp_char = sArr[i].ToCharArray();
+            for(int j=0; j< temp_char.Length/2; j++)
+            {
+                if(temp_char[j] != temp_char[temp_char.Length - 1 - j])
+                {
+                    result[i]+= Math.Abs(temp_char[j] - temp_char[ temp_char.Length-1-j]);
+                }
+               
+
+            }
+            Console.WriteLine(result[i]);
+        }
+        
+
+
+    }
+}
