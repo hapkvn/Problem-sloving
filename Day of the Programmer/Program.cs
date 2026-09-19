18848dd2e19df55ade10c14f604561158bd8c423
hapkvn <huyhoangpkvn75@gmail.com>
2026-09-19 23:03:34 +0700

    Day of the Programmer

---
+++ Day of the Programmer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Program
+{
+    public static void Main()
+    {
+        int year = int.Parse(Console.ReadLine());
+
+        
+        if(year%4 == 0 && year%100 != 0 || year%400 == 0)
+        {
+            Console.WriteLine("12.09."+year);
+        }
+        // else if(year%400 == 0)
+        // {
+        //     Console.WriteLine("12.09."+year);
+        // }
+        else if(year < 1918 && year %4==0) 
+        {
+            Console.WriteLine("12.09."+year);
+        }
+        
+        else if(year == 1918)
+        {
+            Console.WriteLine("26.09."+year);
+        }
+        
+        else
+        {
+            Console.WriteLine("13.09."+year);   
+
+        }
+    }
+}
