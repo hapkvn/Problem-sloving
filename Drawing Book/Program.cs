13505055965abc07da6fe53f1dd1068202709241
hapkvn <huyhoangpkvn75@gmail.com>
2026-09-19 23:45:38 +0700

    Drawing Book

---
+++ Drawing Book/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int p = int.Parse(Console.ReadLine());       
+        
+
+        int begin = p/2;
+
+        int end = n/2;
+
+        int form_end = end-begin;
+
+        if(form_end>= begin)
+        {
+            Console.WriteLine(begin);
+        }
+        else
+        {
+         Console.WriteLine(form_end);
+  
+        }
+
+    }
+}
