761adc8e9244aacc38cc3281d5cfd38cab2bd15c
hapkvn <huyhoangpkvn75@gmail.com>
2026-09-23 14:42:00 +0700

    Pangrams

---
+++ Pangrams/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class Program
+{
+    public static void Main()
+    {
+        
+
+        string input_string = Console.ReadLine().ToLower();
+
+        int [] abc = new int[27];
+
+
+        foreach(char c in input_string)
+        {
+            if(c>= 'a' && c<='z')
+            {
+               int temp = c-'a';
+               abc[temp]++;
+            }
+            if(c == ' ')
+            {
+                abc[26]++;
+            }
+        }
+
+        if (abc.Min() > 0)
+        {
+            Console.WriteLine("pangram");
+        }
+        else
+        {
+            Console.WriteLine("not pangram");
+        }
+    }
+}
